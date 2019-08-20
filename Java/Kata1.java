a3fadfdfbd07d5fef936971f6d9d6ab482c091f5
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-08-19 18:14:25 -0700

    Create Kata1.java

---
+++ Java/Kata1.java
@@ -0,0 +1,25 @@
+/*
+
+Find the highest and Lowest number in a given  String
+
+
+*/
+
+import java.util.Arrays;
+
+public class Kata {
+    public static String highAndLow(String numbers) {
+
+        int min = Arrays.stream(numbers.split(" "))
+                        .mapToInt(i -> Integer.parseInt(i))
+                        .min()
+                        .getAsInt();
+
+        int max = Arrays.stream(numbers.split(" "))
+                        .mapToInt(i -> Integer.parseInt(i))
+                        .max()
+                        .getAsInt();
+
+        return String.format("%d %d", max, min);
+    }
+}
