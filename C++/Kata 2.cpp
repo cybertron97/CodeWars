d89642e168c37edc2243b5aded57fb9212c71cdb
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-08-19 17:32:45 -0700

    Create Kata 2.cpp

---
+++ C++/Kata 2.cpp
@@ -0,0 +1,14 @@
+/*Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).*/
+
+
+
+namespace Triangle
+{
+  bool isTriangle(int a, int b, int c)
+  {
+    return a-b<c && b-c<a && c-a<b;
+
+}
+};
