946fb11d8db3dc18e865bb4d6e9da05ede5fd3c7
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-08-24 21:51:55 -0700

    Create Kata2.java

---
+++ Java/Kata2.java
@@ -0,0 +1,24 @@
+/*
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+*/
+public class Positive{
+
+  public static int sum(int[] arr){
+      int sum1 =0; 
+        for (int i =0; i<arr.length; i++) 
+          {
+            if (arr[i]>0) 
+            {
+              sum1 +=arr[i];
+            }
+          }
+    return sum1;
+  }
+
+}
